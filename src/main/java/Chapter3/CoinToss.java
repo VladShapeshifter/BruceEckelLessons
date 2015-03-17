7e21e8344f1eb68362ad3ecbcd85816c0e7988a4
Vladislav <vladislav.borsch@gmail.com>
2015-03-17 11:44:54 +0200

    done

---
+++ src/main/java/Chapter3/CoinToss.java
@@ -0,0 +1,19 @@
+package Chapter3;
+
+import java.util.Random;
+
+public class CoinToss {
+    static boolean result;
+    static int i;
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        i = rand.nextInt(100);
+        if (i<50) {
+            result = false;
+        } else {
+            result = true;
+        }
+        System.out.println(result);
+    }
+}
