e23724cf4926fe916d6dd866c62c84a320d70c4b
VladShapeshifter <vladislav.borsch@gmail.com>
2015-03-24 21:01:06 +0200

    done

--- src/main/java/Chapter3/LogicalString.java
+++ src/main/java/Chapter3/LogicalString.java
@@ -1,8 +1,8 @@
 package Chapter3;
 
 public class LogicalString {
+    String string;
     static void equalString(String s1, String s2) {
-        String result = "";
         System.out.println(s1.equals(s2));
         System.out.println(s1 == s2);
         System.out.println(s1 != s2);
@@ -12,6 +12,12 @@ public class LogicalString {
         String s1 = "qqq";
         String s2 = "ppp";
         equalString(s1, s2);
+        LogicalString ls1 = new LogicalString();
+        LogicalString ls2 = new LogicalString();
+        ls1.string = "aaa";
+        ls2.string = "bbb";
+        ls2 = ls1;
+        equalString(ls1.string, ls2.string);
 
     }
 }
